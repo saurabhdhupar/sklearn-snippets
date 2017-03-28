5c7edb5d4e410dee4860a9ddaf2bbbfbd22a2adf
Saurabh <saurabh.dhupar@gmail.com>
2017-03-28 00:01:27 -0700

    Notes on how to split data between train and test

---
+++ traintestsplit.py
@@ -0,0 +1,20 @@
+#sklearn-0.17
+#How to create a train test split
+#organizing imports
+from sklearn import datasets
+from sklearn import cross_validation
+
+iris = datasets.load_iris()
+#Parameters to remember is test_size and random_state
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(iris.data, iris.target, test_size=0.4, random_state=0)
+
+
+#sklearn-0.18
+#How to create a train test split
+#organizing imports
+from sklearn import datasets
+from sklearn import model_selection
+
+iris = datasets.load_iris()
+#Parameters to remember is test_size and random_state
+X_train, X_test, y_train, y_test = model_selection.train_test_split(iris.data, iris.target, test_size=0.4, random_state=0)
