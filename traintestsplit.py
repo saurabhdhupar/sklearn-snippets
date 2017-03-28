ad1b4e00b7c544e9aa97d5766b9d59516766c220
Saurabh <saurabh.dhupar@gmail.com>
2017-03-28 00:03:05 -0700

    Adding scikit learn page link based on version

--- traintestsplit.py
+++ traintestsplit.py
@@ -1,6 +1,7 @@
 #sklearn-0.17
 #How to create a train test split
 #organizing imports
+#http://scikit-learn.org/0.17/modules/cross_validation.html
 from sklearn import datasets
 from sklearn import cross_validation
 
@@ -12,6 +13,7 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(iris.data, 
 #sklearn-0.18
 #How to create a train test split
 #organizing imports
+#http://scikit-learn.org/0.18/modules/cross_validation.html
 from sklearn import datasets
 from sklearn import model_selection
 
